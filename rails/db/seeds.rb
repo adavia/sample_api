10c0885774486c7554fad61b1354aacf28ec456b
Andrés Da Viá <andresi.davia@gmail.com>
2016-04-08 11:20:46 -0500

    Add post model and post serializer

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+50.times do |i|
+  Post.create!(
+    content: Faker::Lorem.paragraphs
+  )
+end
